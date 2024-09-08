405238e12ffc9e5191874c55e24d738daa01be1c
Akshay <rokadeakshay5555@gmail.com>
2024-09-08 18:48:19 +0530

    change origin

--- index.js
+++ index.js
@@ -21,7 +21,8 @@ app.use(express.urlencoded({
 app.use(express.json());
 app.use(cookieParser());
 const corsOptions = {
-    origin: "https://66dd590d19010d5bbb662b01--jovial-tarsier-48cadc.netlify.app/login/",
+    // origin: "https://66dd590d19010d5bbb662b01--jovial-tarsier-48cadc.netlify.app",
+    origin: "*",
     credentials: true
 }
 app.use(cors(corsOptions));
