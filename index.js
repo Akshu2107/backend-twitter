285267cf156f8b7dc56f1123139c8ad5bdf2be27
Akshay <rokadeakshay5555@gmail.com>
2024-09-24 19:15:39 +0530

    change origin

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.use(express.urlencoded({
 app.use(express.json());
 app.use(cookieParser());
 const corsOptions = {
-    origin: "https://66dd590d19010d5bbb662b01-jovial-tars.netlify.app",
+    origin: "https://twitterclone123.netlify.app",
     credentials: true
 }
 app.use(cors(corsOptions));
